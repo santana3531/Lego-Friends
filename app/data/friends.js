38f812aeadf0bf4c3a86ab0913706f5e5b8998e9
Art Santana <Art Santana>
2018-02-28 21:22:40 -0500

    server.js & friends

---
+++ app/data/friends.js
@@ -0,0 +1,117 @@
+var homies = [
+    {
+      name: "Hard Hat",
+      photo: "https://randomuser.me/api/portraits/lego/4.jpg",
+      pts: [
+          "4",
+          "2",
+          "3",
+          "5",
+          "1",
+          "2",
+          "3",
+          "3",
+          "3",
+          "2"
+      ]
+    },
+    {
+      name: "Baldy",
+      photo: "https://randomuser.me/api/portraits/lego/1.jpg",
+      pts: [
+          "5",
+          "5",
+          "4",
+          "2",
+          "1",
+          "1",
+          "3",
+          "5",
+          "1",
+          "4"
+      ]
+    },
+    {
+      name: "Dr. Foureyes",
+      photo: "https://randomuser.me/api/portraits/lego/3.jpg",
+      pts: [
+          "3",
+          "4",
+          "3",
+          "3",
+          "2",
+          "2",
+          "1",
+          "1",
+          "5",
+          "5"
+      ]
+    },
+    {
+      name: "Abe Lincoln Jr.",
+      photo: "https://randomuser.me/api/portraits/lego/2.jpg",
+      pts: [
+          "2",
+          "2",
+          "2",
+          "5",
+          "4",
+          "5",
+          "3",
+          "4",
+          "5",
+          "1"
+      ]
+    },
+    {
+      name: "Officer PoPo",
+      photo: "https://randomuser.me/api/portraits/lego/0.jpg",
+      pts: [
+          "1",
+          "1",
+          "1",
+          "2",
+          "3",
+          "3",
+          "3",
+          "2",
+          "3",
+          "3"
+      ]
+    },
+    {
+      name: "Hector Guac",
+      photo: "https://randomuser.me/api/portraits/lego/6.jpg",
+      pts: [
+          "2",
+          "3",
+          "2",
+          "5",
+          "5",
+          "4",
+          "1",
+          "1",
+          "2",
+          "2"
+      ]
+    },
+    {
+      name: "Chef Moostache",
+      photo: "https://randomuser.me/api/portraits/lego/8.jpg",
+      pts: [
+          "2",
+          "3",
+          "2",
+          "5",
+          "5",
+          "4",
+          "1",
+          "1",
+          "2",
+          "2"
+      ]
+    }
+];
+
+
+module.exports = homies;
---
+++ server.js
@@ -0,0 +1,31 @@
+// ***** Dependecies **** 
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// **** Sets up express ****
+
+var app = express();
+
+
+// set up initial port
+
+var PORT = process.env.PORT || 3000;
+
+
+// BodyParse data for server to recieve 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+
+
+// Routing 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Listener
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
